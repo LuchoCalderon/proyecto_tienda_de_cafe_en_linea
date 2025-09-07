fa382059d140962cbc05ce561395b285dbe7fb1f
Luis <luchocalderon16@gmail.com>
2025-09-07 18:05:29 -0500

    adecuacion responsive en el navbar, e imagenes de categoria en home

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Cargar la barra de navegación
+    fetch('navbar.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+      });
+    
+    // Cargar el pie de página
+    fetch('footer.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+      });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+  const navbarCollapse = document.querySelector('.navbar-collapse');
+  
+  navLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+      if (window.innerWidth < 992) {
+        const bsCollapse = new bootstrap.Collapse(navbarCollapse);
+        bsCollapse.hide();
+      }
+    });
+  });
+});
